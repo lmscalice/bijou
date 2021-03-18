07a67a817554b22015c6f9275637bd97f7a30e78
lmscalice <lmscalice@gmail.com>
2021-03-17 22:33:21 -0400

    working start to discover page

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:async/async.dart';
+//when connect auth:
+//final FirebaseUser user = await _firebaseAuth.currentUser();
+//return await FirebaseDatabase.instance.reference().child('user').equalTo(user.uid);
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,15 +31,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,68 +38,112 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool start = true;
+  List<bool> _selections = List.generate(2, (int index) => !(index==1));
+  List<String> dbResultSet;
+  var tempSearchStore = [];
+  var discoverStore = [];
+  final AsyncMemoizer _memoizer = AsyncMemoizer();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future getPageInfo() async 
+  {
+    return this._memoizer.runOnce(() async 
+    {
+      var ref = await FirebaseDatabase.instance.reference().child('Customers/1234567890/Searches/');
+      
+      ref.once().then((DataSnapshot snapshot) async {
+        
+        dbResultSet = snapshot.value.cast<String>();
+
+        await FirebaseFirestore.instance.collection("Products").where('Keys', arrayContainsAny: dbResultSet).get().then((querySnapshot) => 
+        {
+            for (int i = 0; i < querySnapshot.docs.length; i++) {
+                tempSearchStore.add(querySnapshot.docs.elementAt(i).data()),
+                setState(() {
+                  discoverStore.add(tempSearchStore[i]);
+                  print("From:");
+                  print(discoverStore[i]);
+                })
+            }   
+        });
+      });
+    });  
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return FutureBuilder(
+      future: getPageInfo(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done && discoverStore.isNotEmpty) {
+          return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: ListView(children: <Widget>[
+        Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            child:ToggleButtons(
+              children: <Widget>[
+                Icon(Icons.ac_unit),
+                Icon(Icons.call),
+              ],
+              isSelected: _selections,
+              onPressed: (int index) {
+                setState(() {
+                  for (int buttonIndex = 0; buttonIndex < _selections.length; buttonIndex++) {
+                    if (buttonIndex == index) {
+                      print("Running here?");
+                      _selections[buttonIndex] = true;
+                      getPageInfo();
+                    } else {
+                      _selections[buttonIndex] = false;
+                    }
+                  }
+                });
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      GridView.count(
+        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            crossAxisCount: 2,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+            primary: false,
+            shrinkWrap: true,
+            children: discoverStore.map((element) {
+              return buildResultCard(element);
+            }).toList()
+      )
+    ])      
     );
+        }
+        else {
+          return Center(
+            child: CircularProgressIndicator()
+          );
+        }
+      }
+    );
+    
   }
+}
+
+Widget buildResultCard(data) {
+  return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      elevation: 2.0,
+      child: Container(
+          child: Center(
+        child: Text(
+          data['Name'],
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+          ),
+        ),
+      )));
 }
